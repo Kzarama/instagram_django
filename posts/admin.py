8c45d7f952365b68ed9f890b49069d91b53e2bd2
kz <zaramaluna1999@hotmail.com>
2020-06-02 17:27:57 -0500

    project added

---
+++ posts/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from posts.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'profile', 'title', 'photo')
+    list_display_links = ('pk', 'user')
+    search_fields = ('user__username', 'title')
+    list_filter = ('created', 'modified', 'user__is_active', 'user__is_staff')
+    readonly_fields = ('created', 'modified', 'user')
+    fieldsets = (
+        ('Post',{
+            'fields' : (('title', 'photo'))
+        }),
+        ('Metadata', {
+            'fields':(
+                ('user',),
+                ('created', 'modified'),
+            )
+        })
+    )
